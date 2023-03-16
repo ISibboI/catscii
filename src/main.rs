1c2afba00315ea7036aec0cd3987dcf6b7f7c982
Sebastian <isibboi@gmail.com>
2023-03-16 13:25:11 +0000

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,29 @@ use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let response = reqwest::get("https://api.thecatapi.com/v1/images/search")
-        .await
-        .unwrap();
-    println!("Status: {}", response.status());
+    let url = get_cat_image_url().await.unwrap();
+    println!("The image is at {}", url);
+}
+
+async fn get_cat_image_url() -> color_eyre::Result<String> {
+    let api_url = "https://api.thecatapi.com/v1/images/search";
+    let res = reqwest::get(api_url).await?;
+    if !res.status().is_success() {
+        return Err(color_eyre::eyre::eyre!(
+            "The Cat API returned HTTP {}",
+            res.status()
+        ));
+    }
 
     #[derive(Deserialize)]
     struct CatImage {
         url: String,
     }
+    let images: Vec<CatImage> = res.json().await?;
+    // this syntax is new in Rust 1.65
+    let Some(image) = images.into_iter().next() else {
+        return Err(color_eyre::eyre::eyre!("The Cat API returned no images"));
+    };
 
-    let images: Vec<CatImage> = response.json().await.unwrap();
-    let image = images.first().expect("The cat API should return at least one image.");
-    println!("The image is at {}", image.url);
+    Ok(image.url)
 }
