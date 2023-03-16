a7f45423b1d239819dab763cbb5b714334f0f215
Sebastian <isibboi@gmail.com>
2023-03-16 13:11:56 +0000

    Readme.

---
+++ README.md
@@ -0,0 +1,4 @@
+# catscii
+
+Serves cat pictures as ASCII art over the internet.
+Tutorial project from [this](https://fasterthanli.me/series/building-a-rust-service-with-nix) tutorial.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let response = reqwest::get("https://api.thecatapi.com/v1/images/search")
+        .await
+        .unwrap();
+    println!("Status: {}", response.status());
+    let body = response.text().await.unwrap();
+    println!("Body: {}", body);
 }
