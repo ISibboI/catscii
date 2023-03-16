601ca387ac0dbb6d70c10f740175b2721a244522
Sebastian <isibboi@gmail.com>
2023-03-16 13:33:11 +0000

    Hexdump image.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+use pretty_hex::PrettyHex;
+use reqwest::Client;
 use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let url = get_cat_image_url().await.unwrap();
-    println!("The image is at {}", url);
+    let image_bytes = get_cat_image_bytes(&Default::default()).await.unwrap();
+    println!("The image is {:?}", image_bytes[..200].hex_dump());
 }
 
-async fn get_cat_image_url() -> color_eyre::Result<String> {
+async fn get_cat_image_bytes(client: &Client) -> color_eyre::Result<Vec<u8>> {
+    Ok(client.get(get_cat_image_url(client).await?).send().await?.error_for_status()?.bytes().await?.to_vec())
+}
+
+async fn get_cat_image_url(client: &Client) -> color_eyre::Result<String> {
     let api_url = "https://api.thecatapi.com/v1/images/search";
-    let res = reqwest::get(api_url).await?;
+    let res = client.get(api_url).send().await?;
     if !res.status().is_success() {
         return Err(color_eyre::eyre::eyre!(
             "The Cat API returned HTTP {}",
