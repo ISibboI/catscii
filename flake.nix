a0a72c793ee8a34959c825b747ba2fdc5b526b59
Sebastian Schmidt <isibboi@gmail.com>
2023-07-16 15:01:35 +0000

    Cargo build stuff.

--- flake.nix
+++ flake.nix
@@ -9,8 +9,16 @@
         flake-utils.follows = "flake-utils";
       };
     };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
-  outputs = {self, nixpkgs, flake-utils, rust-overlay}:
+  outputs = {self, nixpkgs, flake-utils, rust-overlay, crane}:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
@@ -19,13 +27,24 @@
             inherit system overlays;
           };
           rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+          src = craneLib.cleanCargoSource ./.;
           nativeBuildInputs = with pkgs; [rustToolchain pkg-config];
           buildInputs = with pkgs; [openssl];
+          commonArgs = {
+            inherit src buildInputs nativeBuildInputs;
+          };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          bin = craneLib.buildPackage(commonArgs // {inherit cargoArtifacts;});
         in
         with pkgs;
         {
+          packages = {
+            inherit bin;
+            default = bin;
+          };
           devShells.default = mkShell {
-            inherit buildInputs nativeBuildInputs;
+            inputsFrom = [bin];
           };
         }
       );
